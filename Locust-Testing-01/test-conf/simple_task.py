74de9a6defc13377d2e29e77a541956984c578ec
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-14 12:15:24 +0300

    Adding event to request, stats, init, quitting, and spawn

--- Locust-Testing-01/test-conf/simple_task.py
+++ Locust-Testing-01/test-conf/simple_task.py
@@ -1,4 +1,32 @@
-from locust import task, HttpUser, constant
+from locust import task, HttpUser, constant, events
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+@events.spawning_complete.add_listener
+def spawn_users(count_users, **kwargs):
+    print(f"All users spawn successfully with count: {count_users}")
+
+def on_request_success(request_type, name, response_time, response_length, **kwargs):
+    if kwargs.get("exception") is None:
+        logger.info(f"Request succeeded: {request_type} {name} took {response_time}ms")
+def on_request_failure(request_type, name, response_time, exception, **kwargs):
+        logger.error(f"Request failed: {request_type} {name} took {response_time}ms with error {exception}")
+
+@events.init.add_listener
+def on_locust_init(environment, **kwargs):
+    # This event is fired when the Locust environment is initialized
+    # We can listen for the request event and check for success or failure.
+    events.request.add_listener(on_request_success)
+    events.request.add_listener(on_request_failure)
+
+@events.quitting.add_listener
+def check_res(environment, **kwargs):
+    if environment.stats.total.fail_ratio > 0.5:
+        logger.error("We have many failures")
+    else:
+        logger.info("Failure Rate is low")
 
 class SimpleTask(HttpUser):
     wait_time = constant(1)
@@ -10,9 +38,9 @@ class SimpleTask(HttpUser):
     @task
     def exist(self):
         response = self.client.get('/', name=self.target)
-        print(response.status_code)
+        # print(response.status_code)
     
     @task
     def notExist(self):
         response = self.client.get('/loka.html', name=self.target + '/notExist')
-        print(response.status_code)
+        # print(response.status_code)
